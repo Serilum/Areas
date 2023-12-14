fe6bd7e23ea026226d8511d8df85113121879d81
jonko0493 <jonko0493@protonmail.com>
2023-12-14 09:19:25 -0800

    Add BlueMap integration

--- Common/src/main/java/com/natamus/areas/ModCommon.java
+++ Common/src/main/java/com/natamus/areas/ModCommon.java
@@ -1,8 +1,10 @@
 package com.natamus.areas;
 
 import com.natamus.areas.config.ConfigHandler;
+import com.natamus.areas.integrations.BlueMapIntegration;
 import com.natamus.areas.util.Reference;
 import com.natamus.collective.config.GenerateJSONFiles;
+import de.bluecolored.bluemap.api.BlueMapAPI;
 
 public class ModCommon {
 
@@ -13,5 +15,10 @@ public class ModCommon {
 
 	private static void load() {
 		GenerateJSONFiles.requestJSONFile(Reference.MOD_ID, "area_names.json");
+//		try {
+//			BlueMapAPI.onEnable(BlueMapIntegration::updateBlueMap);
+//		} catch (NoClassDefFoundError | IllegalStateException ignored) {
+//			System.out.println("BlueMap is not loaded");
+//		}
 	}
 }
