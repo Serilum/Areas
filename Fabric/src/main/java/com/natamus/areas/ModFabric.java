b327fd9221de9b7de7017c6f55280c55cd98677a
Rick South <rick@natamus.com>
2024-07-28 07:44:10 +0200

    (v6.0) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/areas/ModFabric.java
+++ Fabric/src/main/java/com/natamus/areas/ModFabric.java
@@ -3,6 +3,7 @@ package com.natamus.areas;
 import com.natamus.areas.events.AreaEvent;
 import com.natamus.areas.util.Reference;
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.collective.fabric.callbacks.CollectiveBlockEvents;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
@@ -18,6 +19,10 @@ public class ModFabric implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		setGlobalConstants();
 		ModCommon.init();
 
--- Fabric/src/main/java/com/natamus/areas/ModFabricClient.java
+++ Fabric/src/main/java/com/natamus/areas/ModFabricClient.java
@@ -3,6 +3,8 @@ package com.natamus.areas;
 import com.natamus.areas.events.ClientEvent;
 import com.natamus.areas.fabric.cmds.FabricClientCommandAreas;
 import net.fabricmc.api.ClientModInitializer;
+import com.natamus.areas.util.Reference;
+import com.natamus.collective.check.ShouldLoadCheck;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.Minecraft;
@@ -10,6 +12,10 @@ import net.minecraft.client.Minecraft;
 public class ModFabricClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() { 
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		registerEvents();
 	}
 	
