6ad987dc3d4ccc6044a471a909da205c1f8ad482
Rick South <rick@natamus.com>
2025-07-18 03:13:55 +0200

    Areas source code for 1.21.8.

---
+++ Forge/src/main/java/com/natamus/areas/forge/events/ForgeAreaEvent.java
@@ -0,0 +1,39 @@
+package com.natamus.areas.forge.events;
+
+import com.natamus.areas.events.AreaEvent;
+import com.natamus.collective.functions.WorldFunctions;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.TickEvent.LevelTickEvent;
+import net.minecraftforge.event.TickEvent.Phase;
+import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+
+import java.lang.invoke.MethodHandles;
+
+public class ForgeAreaEvent {
+	public static void registerEventsInBus() {
+		BusGroup.DEFAULT.register(MethodHandles.lookup(), ForgeAreaEvent.class);
+	}
+
+	@SubscribeEvent
+	public static void onWorldTick(LevelTickEvent.Post e) {
+		Level level = e.level;
+		if (level.isClientSide) {
+			return;
+		}
+
+		AreaEvent.onWorldTick((ServerLevel)level);
+	}
+
+	@SubscribeEvent
+	public static void onNeighbourNotice(BlockEvent.NeighborNotifyEvent e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		AreaEvent.onNeighbourNotice(level, e.getPos(), e.getState(), e.getNotifiedSides(), e.getForceRedstoneUpdate());
+	}
+}
---
+++ NeoForge/src/main/java/com/natamus/areas/neoforge/events/NeoForgeClientEvent.java
@@ -0,0 +1,21 @@
+package com.natamus.areas.neoforge.events;
+
+import com.natamus.areas.cmds.ClientCommandAreas;
+import com.natamus.areas.data.ClientConstants;
+import com.natamus.areas.events.ClientEvent;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.client.event.RegisterClientCommandsEvent;
+
+public class NeoForgeClientEvent {
+	@SubscribeEvent
+	public static void onClientTick(ClientTickEvent.Pre e) {
+		ClientEvent.onClientTick(ClientConstants.mc);
+	}
+
+	@SubscribeEvent
+	public static void registerCommands(RegisterClientCommandsEvent e) {
+		ClientCommandAreas.register(e.getDispatcher());
+	}
+}
